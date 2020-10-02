b75f9b413a21a11faa0d1008b6503d5f1b0aa170
ALina Ulyanova <ulyanova.alina1997@gmail.com>
2020-10-02 10:38:12 +0300

    feat: implement 3 functions: max, min, and avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(!array || array.length === 0) return 0;
+
+  return array.reduce((a, b) => a < b ? a : b);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(!array || array.length === 0) return 0;
+
+  return array.reduce((a, b) => a > b ? a : b);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!array || array.length === 0) return 0;
+
+  const sum = array.reduce((a, b) => a + b);
+  return sum / array.length;
 }
